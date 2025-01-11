0e34576da362b100e3bad8e51897a0c640693745
syamasaw <17191sawa@gmail.com>
2025-01-11 17:10:39 +0900

    fix: nullptrがc++98で使えないのでNULLに変更

--- cmd/ft_irc/main.cpp
+++ cmd/ft_irc/main.cpp
@@ -6,7 +6,7 @@
 /*   By: syamasaw <syamasaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 15:12:59 by syamasaw          #+#    #+#             */
-/*   Updated: 2025/01/05 15:50:46 by syamasaw         ###   ########.fr       */
+/*   Updated: 2025/01/11 17:10:05 by syamasaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,19 +31,19 @@ int main(int argc, char **argv) {
 Server *processArgs(int argc, char **argv) {
   if (argc != 3) {
     std::cerr << "Usage: " << argv[0] << " <port> <password>" << std::endl;
-    return nullptr;
+    return NULL;
   }
   std::stringstream ss(argv[1]);
   unsigned int port;
   ss >> port;
   if (ss.fail() || port > 65535) {
     std::cerr << "Invalid port" << std::endl;
-    return nullptr;
+    return NULL;
   }
   std::string password(argv[2]);
   if (password.empty() || (password.find(' ') != std::string::npos)) {
     std::cerr << "Invalid password" << std::endl;
-    return nullptr;
+    return NULL;
   }
   return new Server(port, password);
 }
