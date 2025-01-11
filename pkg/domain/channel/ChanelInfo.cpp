2d1671e8f0cfc809da5bd24d028631294b4186a6
GawinGowin <>
2025-01-11 11:33:14 +0000

    update(pkg/channel): チャンネル名のバリデーションを作成

--- pkg/domain/channel/ChanelInfo.cpp
+++ pkg/domain/channel/ChanelInfo.cpp
@@ -1,8 +1,27 @@
 #include "ChanelInfo.hpp"
 
+const std::list<std::string> forbiddenChars({":", ",", " ", "\a"});
+const std::list<std::string> channelNameInitialChars({"#", "&", "+", "!"});
+
 Channel::Channel(): _name(""), _topic("") {}
 
-Channel::Channel(std::string name, std::string topic): _name(name), _topic(name) {}
+Channel::Channel(std::string name, std::string topic): _name(name), _topic(name) {
+  if (this->_name.empty()) {
+    throw std::invalid_argument("Channel name cannot be empty");
+  }
+  if (this->_name.size() > 50) {
+    throw std::invalid_argument("Channel name cannot be longer than 50 characters");
+  }
+  if (this->_name.at(0) != '#' && this->_name.at(0) != '&' && this->_name.at(0) != '+' && this->_name.at(0) != '!') {
+    throw std::invalid_argument("Channel name must start with #, &, + or !");
+  }
+  std::list<std::string>::const_iterator it;
+  for (it = forbiddenChars.begin(); it != forbiddenChars.end(); ++it) {
+    if (this->_name.find(*it) != std::string::npos) {
+      throw std::invalid_argument("Channel name cannot contain ':', ',', ' ', or '\a'");
+    }
+  }
+}
 
 Channel::Channel(const Channel &other) {
   *this = other;
--- pkg/domain/channel/ChanelInfo.hpp
+++ pkg/domain/channel/ChanelInfo.hpp
@@ -2,16 +2,18 @@
 #define CHANELINFO_HPP
 
 #include <string>
+#include <stdexcept>
+#include <list>
 
 class Channel {
 public:
-    Channel();
+    Channel(std::string name, std::string topic);
     Channel(const Channel &other);
     Channel &operator=(const Channel &other);
-    Channel(std::string name, std::string topic);
     std::string getName() const;
     std::string getTopic() const;
 private:
+    Channel();
     std::string _name;
     std::string _topic;
 };
