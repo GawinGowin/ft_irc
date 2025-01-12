5bdbedb14734200a8010b8b9bce7f47dbd786503
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2025-01-12 00:08:55 +0000

    wip

--- pkg/domain/channel/ChannelMode.hpp
+++ pkg/domain/channel/ChannelMode.hpp
@@ -1,8 +1,21 @@
 #ifndef CHANNELMODE_HPP
 #define CHANNELMODE_HPP
 
+#include <list>
+#include <string>
+
+typedef std::list<std::string> SpecifiedChars;
+
+enum ChannelInitialChar {
+  CHANNEL_STANDARD = '#',
+  CHANNEL_LOCAL = '&',
+  CHANNEL_DISABLED = '+',
+  CHANNEL_PRIVATE = '!'
+};
+
 class ChannelMode {
-  
+  private:
+    const SpecifiedChars channelNameInitialChars({"#", "&", "+", "!"});
 };
 
 #endif /* CHANNELMODE_HPP */
