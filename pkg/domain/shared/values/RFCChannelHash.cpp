023bb156af53544a3e8275c17d4e1a8398c5ee8a
GawinGowin <>
2025-01-30 18:11:00 +0000

    update: RFC 2811 5.2.1で提示されたハッシュ化を実装

---
+++ pkg/domain/shared/values/RFCChannelHash.cpp
@@ -0,0 +1,46 @@
+#include "domain/shared/values/RFCChannelHash.hpp"
+
+static const std::string base = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+static const int channelIdLength = 5;
+
+RFCChannelHash::RFCChannelHash(const std::time_t &current_time) {
+  this->_hashedValue = static_cast<long>(current_time);
+}
+
+RFCChannelHash::RFCChannelHash(const RFCChannelHash &obj) { *this = obj; }
+
+RFCChannelHash &RFCChannelHash::operator=(const RFCChannelHash &obj) {
+  if (this != &obj) {
+    this->_hashedValue = obj._hashedValue;
+  }
+  return *this;
+}
+
+RFCChannelHash::~RFCChannelHash() {}
+
+bool RFCChannelHash::operator==(const IHashAggregateRoot &other) const {
+  return this->_hashedValue == other.getHashLong();
+}
+
+std::string RFCChannelHash::getHash() const {
+  int raw_id = this->_toHash(this->_hashedValue);
+  size_t base_size = base.size();
+  std::ostringstream oss;
+  for (int i = 0; i < channelIdLength; ++i) {
+    oss << base[raw_id % base_size];
+    raw_id /= base_size;
+  }
+  return oss.str();
+}
+
+long RFCChannelHash::getHashLong() const { return this->_hashedValue; }
+
+int RFCChannelHash::getHashInt() const {
+  return this->_toHash(this->_hashedValue);
+}
+
+int RFCChannelHash::_toHash(const std::time_t &current_time) const {
+  const int base_size = static_cast<int>(base.size());
+  long raw_id = current_time % static_cast<long>(std::pow(base_size, channelIdLength));
+  return static_cast<int>(raw_id);
+}
---
+++ pkg/domain/shared/values/RFCChannelHash.hpp
@@ -0,0 +1,28 @@
+#ifndef RFCCHANNELHASH_HPP
+#define RFCCHANNELHASH_HPP
+
+#include "domain/shared/values/IHashAggregateRoot.hpp"
+#include <cmath>
+#include <ctime>
+#include <sstream>
+#include <string>
+
+class RFCChannelHash : virtual public IHashAggregateRoot {
+public:
+  RFCChannelHash(const std::time_t &current_time);
+  RFCChannelHash(const RFCChannelHash &obj);
+  RFCChannelHash &operator=(const RFCChannelHash &obj);
+  ~RFCChannelHash();
+
+  bool operator==(const IHashAggregateRoot &other) const;
+  std::string getHash() const;
+  long getHashLong() const;
+  int getHashInt() const;
+
+private:
+  RFCChannelHash();
+  int _toHash(const std::time_t &current_time) const;
+  long _hashedValue;
+};
+
+#endif /* RFCCHANNELHASH_HPP */
