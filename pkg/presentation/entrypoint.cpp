0f0b6cb89888bb4781ef19ab9758eb43c5afca84
GawinGowin <>
2025-02-02 23:59:22 +0000

    fix(entrypoint): usecaseの使用順序を修正した

--- pkg/presentation/entrypoint.cpp
+++ pkg/presentation/entrypoint.cpp
@@ -1,16 +1,15 @@
 #include "presentation/entrypoint.hpp"
 
 void entrypoint(int argc, char **argv) {
-  MonitorSocketEventsUseCase monitorSocketEventsUseCase;
-  MonitorSocketEventDTO eventDto;
   StartServerDTO dto(argc, argv);
   StartServerUseCase startServerUseCase(dto);
-
   try {
     startServerUseCase.execute();
   } catch (const std::runtime_error &e) {
     throw std::runtime_error(std::string("Failed to start server: ") + e.what());
   }
+  MonitorSocketEventsUseCase monitorSocketEventsUseCase;
+  MonitorSocketEventDTO eventDto;
   std::cout << "Start Listening..." << std::endl;
   while (true) {
     eventDto = monitorSocketEventsUseCase.monitor();
