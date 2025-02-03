971cb6325ffa28dfc72fa06a662c917266a7ab66
GawinGowin <>
2025-02-03 13:35:44 +0000

    add(domain): Messageクラスの追加

---
+++ pkg/domain/message/IMessageAggregateRoot.hpp
@@ -0,0 +1,18 @@
+#ifndef IMESSAGEAGGREGATEROOT_HPP
+#define IMESSAGEAGGREGATEROOT_HPP
+
+#include <string>
+#include <vector>
+
+class IMessageAggregateRoot {
+public:
+  enum CommandType { PASS, NICK, USER, JOIN, PRIVMSG, KICK, INVITE, TOPIC, MODE, ERROR, UNKNOWN };
+
+  virtual ~IMessageAggregateRoot(){};
+
+  virtual const std::string &getPrefix() const = 0;
+  virtual const CommandType &getCommand() const = 0;
+  virtual const std::vector<std::string> &getParams() const = 0;
+};
+
+#endif /* IMESSAGEAGGREGATEROOT_HPP */
