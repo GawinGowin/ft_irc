3e45332a2c3f5b3ac2210e1114626f701ea35ce6
syamasaw <138424335+Sawawa42@users.noreply.github.com>
2025-02-15 17:30:30 +0900

    feat: メッセージパーサの実装 (#57)
    * パーサの実装(未リファクタ)
    
    * パーサを移動
    
    * fmt
    
    * testを一部追加
    
    * testを追加
    
    * testを修正
    
    * パーサのコマンド開始index取得部分を修正
    
    * testを修正
    
    * basemessageのテスト追加
    
    * fmt
    
    * cmakelistに追加
    
    ---------
    
    Co-authored-by: GawinGowin <101625248+GawinGowin@users.noreply.github.com>

---
+++ pkg/tests/domain/message/test_Parser.cpp
@@ -0,0 +1,67 @@
+#include "domain/message/Parser.hpp"
+#include <gtest/gtest.h>
+
+TEST(ParserTest, testLongMessage) {
+  // Max length of a message is 512 characters, including the CRLF(2 characters)
+  // So, C is the last character of the message.
+  const std::string strLong = "PRIVMSG #channel " + std::string(490, 'a') + "ABCDEFGH\r\n";
+  Parser parser(strLong);
+
+  EXPECT_EQ(parser.getParams()[1].back(), 'C');
+}
+
+TEST(ParserTest, testNoCRLF) {
+  EXPECT_THROW(Parser("PRIVMSG #channel :Hello, world!"), std::runtime_error);
+  EXPECT_THROW(Parser("\n"), std::runtime_error);
+}
+
+TEST(ParserTest, testPrefix) {
+  Parser prefix(":prefix PRIVMSG #channel :Hello, world!\r\n");
+  Parser noprefix("PRIVMSG #channel :Hello, world!\r\n");
+
+  EXPECT_EQ(prefix.getPrefix(), "prefix");
+  EXPECT_EQ(prefix.getCommand(), IMessageAggregateRoot::PRIVMSG);
+  EXPECT_EQ(prefix.getParams()[0], "#channel");
+  EXPECT_EQ(prefix.getParams()[1], "Hello, world!");
+  EXPECT_EQ(noprefix.getPrefix(), "");
+  EXPECT_THROW(Parser(":prefix\r\n"), std::runtime_error);
+  EXPECT_THROW(Parser(":prefix \r\n"), std::runtime_error);
+}
+
+TEST(ParserTest, testCommand) {
+  Parser pass("PASS password\r\n");
+  Parser nick("NICK nickname\r\n");
+  Parser user("USER username hostname servername :realname\r\n");
+  Parser join("JOIN #channel\r\n");
+  Parser privmsg("PRIVMSG #channel :Hello, world!\r\n");
+  Parser kick("KICK #channel user :reason\r\n");
+  Parser invite("INVITE user #channel\r\n");
+  Parser topic("TOPIC #channel :new topic\r\n");
+  Parser mode("MODE #channel +o user\r\n");
+  Parser error("ERROR\r\n");
+  Parser unknown("hoge huge\r\n");
+
+  EXPECT_EQ(pass.getCommand(), IMessageAggregateRoot::PASS);
+  EXPECT_EQ(nick.getCommand(), IMessageAggregateRoot::NICK);
+  EXPECT_EQ(user.getCommand(), IMessageAggregateRoot::USER);
+  EXPECT_EQ(join.getCommand(), IMessageAggregateRoot::JOIN);
+  EXPECT_EQ(privmsg.getCommand(), IMessageAggregateRoot::PRIVMSG);
+  EXPECT_EQ(kick.getCommand(), IMessageAggregateRoot::KICK);
+  EXPECT_EQ(invite.getCommand(), IMessageAggregateRoot::INVITE);
+  EXPECT_EQ(topic.getCommand(), IMessageAggregateRoot::TOPIC);
+  EXPECT_EQ(mode.getCommand(), IMessageAggregateRoot::MODE);
+  EXPECT_EQ(error.getCommand(), IMessageAggregateRoot::ERROR);
+  EXPECT_EQ(unknown.getCommand(), IMessageAggregateRoot::UNKNOWN);
+}
+
+TEST(ParserTest, testParams) {
+  Parser noParams("PASS\r\n");
+  Parser noParamsAndSpace("PASS   \r\n");
+  Parser trailingParams("PRIVMSG #channel :Hello, world!\r\n");
+  Parser multipleParams("MODE #channel +o user  \r\n");
+
+  EXPECT_EQ(noParams.getParams().size(), 0);
+  EXPECT_EQ(noParamsAndSpace.getParams().size(), 0);
+  EXPECT_EQ(trailingParams.getParams().size(), 2);
+  EXPECT_EQ(multipleParams.getParams().size(), 3);
+}
