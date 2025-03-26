80b125c7aa959ef788094065b5e41d0e909f12fb
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2025-03-26 07:49:59 +0000

    update: Sentryで"パンくずリスト"(breadcrumb)を使用するようにした

--- pkg/infra/logger/SentryLogger.hpp
+++ pkg/infra/logger/SentryLogger.hpp
@@ -15,28 +15,24 @@ public:
     (void)msg; // do nothing
   };
   void debug(std::string msg) {
-    sentry_capture_event(sentry_value_new_message_event(
-        /*   level */ SENTRY_LEVEL_DEBUG,
-        /*  logger */ "custom",
-        /* message */ msg.c_str()));
+    sentry_value_t crumb = sentry_value_new_breadcrumb("default", msg.c_str());
+    sentry_value_set_by_key(crumb, "level", sentry_value_new_string("debug"));
+    sentry_add_breadcrumb(crumb);
   };
   void info(std::string msg) {
-    sentry_capture_event(sentry_value_new_message_event(
-        /*   level */ SENTRY_LEVEL_INFO,
-        /*  logger */ "custom",
-        /* message */ msg.c_str()));
+    sentry_value_t crumb = sentry_value_new_breadcrumb("default", msg.c_str());
+    sentry_value_set_by_key(crumb, "level", sentry_value_new_string("info"));
+    sentry_add_breadcrumb(crumb);
   };
   void warning(std::string msg) {
-    sentry_capture_event(sentry_value_new_message_event(
-        /*   level */ SENTRY_LEVEL_WARNING,
-        /*  logger */ "custom",
-        /* message */ msg.c_str()));
+    sentry_value_t crumb = sentry_value_new_breadcrumb("default", msg.c_str());
+    sentry_value_set_by_key(crumb, "level", sentry_value_new_string("warning"));
+    sentry_add_breadcrumb(crumb);
   };
   void error(std::string msg) {
-    sentry_capture_event(sentry_value_new_message_event(
-        /*   level */ SENTRY_LEVEL_ERROR,
-        /*  logger */ "custom",
-        /* message */ msg.c_str()));
+    sentry_value_t crumb = sentry_value_new_breadcrumb("default", msg.c_str());
+    sentry_value_set_by_key(crumb, "level", sentry_value_new_string("error"));
+    sentry_add_breadcrumb(crumb);
   };
   void fatal(std::string msg) {
     sentry_capture_event(sentry_value_new_message_event(
