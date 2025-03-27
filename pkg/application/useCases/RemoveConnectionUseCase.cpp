f44fca12982a27173e4ff79543b9d8cf9ae68a39
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2025-03-27 11:38:55 +0000

    update: チャンネルに所属していたとき、適切に抜ける処理を追加

--- pkg/application/useCases/RemoveConnectionUseCase.cpp
+++ pkg/application/useCases/RemoveConnectionUseCase.cpp
@@ -3,11 +3,19 @@
 void RemoveConnectionUseCase::remove(int clientFd) {
   SocketHandler *_socketHandler = &SocketHandlerServiceLocator::get();
   InmemoryClientDatabase *db = &InmemoryClientDBServiceLocator::get();
+  MultiLogger *logger = LoggerServiceLocator::get();
   try {
-    MultiLogger *logger = LoggerServiceLocator::get();
+    IClientAggregateRoot *client = db->getByFd(clientFd);
+    if (client == NULL) {
+      throw std::runtime_error("Client not found unexpectedly");
+    }
+    ChannelService::removeClientFromAllChannels(client->getId());
+    logger->debugss() << client->getId() << "(fd:" << clientFd << ") removed from all channels";
+
+    // TODO: ユーザーが空ならば、チャンネルを削除する機構を追加する
+
+    db->remove(client->getId());
     _socketHandler->closeConnection(clientFd);
-    db->removeFdsByFd(clientFd);
-    // TODO: チャンネルからもユーザーを削除する処理を追加
     logger->infoss() << "Connection closed: (fd:" << clientFd << ")";
   } catch (const std::runtime_error &e) {
     throw std::runtime_error(std::string("Remove connection: ") + e.what());
